e0b4dbed8e7b894670da19ecc757674897023e14
Bucky <jacobowens75@gmail.com>
2022-08-22 07:13:58 -0500

    added images

--- pages/locations.js
+++ pages/locations.js
@@ -1,24 +1,40 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import Image from "next/image";
+import locationStyles from "../styles/Location.module.css";
 
 const locations = () => {
   return (
-    <div>
+    <div className={locationStyles.container}>
       <NavBar />
       <h2>Come see us wherever is more convienent for you</h2>
-      <h3>
-        Tupelo location:
-        <p>
-          (Insert Image) located @ <br />
+      <h3 className={locationStyles.tupelo}>
+        <Image
+          className={locationStyles.pics}
+          src="/tupeloFront.png"
+          height={400}
+          width={650}
+          layout="intrinsic"
+          quality={100}
+        />
+        <p className={locationStyles.tupeloInfo}>
+          {" "}
+          Tupelo location: located @ <br />
           609 Brunson Drive <br />
           Tupelo, MS 38801
-        </p>
-        <p>
-          Phone: 662-205-4486 <br />
+          <br /> Phone: 662-205-4486 <br />
           Fax: 662-205-4588
         </p>
       </h3>
       <h3>
+        <Image
+          className={locationStyles.pics}
+          src="/tupeloFront.png"
+          height={400}
+          width={650}
+          layout="intrinsic"
+          quality={100}
+        />
         Corinth location:
         <p>
           (Insert Image) located @ <br />
